780b14fef1fc99068ec0337c64c0001ee195aa53
Fadhil van Halen <rhyn.ml@gmailm.com>
2024-06-10 20:18:07 +0700

    ok

---
+++ api.go
@@ -0,0 +1,51 @@
+package GoApi
+
+import (
+  "bytes"
+  "encoding/json"
+  "log"
+)
+
+type GoApi struct {
+    BaseURL string
+    Apikey  string
+}
+
+func NewPsychopumpumApi(apikey string) *GoApi {
+    client := &PsychopumpumApi{
+        BaseURL: "https://api.psychopumpum.fun/",
+        Apikey : apikey,
+    }
+    return client
+}
+
+func (self *GoApi) request(path string, params map[string]string) *Server {
+    fullUrl := self.urlEncode(self.BaseURL, path, params)
+    server := NewServer(Options{
+        Url: fullUrl,
+        Headers: map[string]string{
+            "Authorization": self.Apikey,
+        },
+    })
+    return server
+}
+
+func (self *GoApi) InstagramPost(url string) map[string]interface{} {
+    cl := self.request("instagram/post/", map[string]string{
+        "url": url,
+    })
+    res, err := cl.Get().Exec()
+    if err != nil {
+        log.Fatal(err)
+    }
+    var result map[string]interface{}
+    body, err := res.Content()
+    if err != nil {
+        log.Fatal(err)
+    }
+    err = json.NewDecoder(bytes.NewReader(body)).Decode(&result)
+    if err != nil {
+        log.Fatal(err)
+    }
+    return result
+}
---
+++ server.go
@@ -0,0 +1,101 @@
+package GoApi
+
+import (
+  "io"
+  "io/ioutil"
+  "net/http"
+  "net/url"
+)
+
+const (
+    GET    = "GET"
+    POST   = "POST"
+)
+
+func (self *GoApi) urlEncode(host, path string, data map[string]string) string {
+    baseurl, _  := url.Parse(host)
+    baseurl.Path = path
+    params := url.Values{}
+    for k, v := range data {
+        params.Add(k, v)
+    }
+    baseurl.RawQuery = params.Encode()
+    return baseurl.String()
+}
+
+type Server struct {
+    url     string
+    headers map[string]string
+    body    io.Reader
+    client  *http.Client
+    req     *http.Request
+}
+
+type Options struct {
+    Url            string
+    Headers        map[string]string
+    Body           io.Reader
+    FollowRedirect bool
+}
+
+type ResponseReader struct {
+    Response *http.Response
+}
+
+func NewServer(opt *Options) *Server {
+    server        := new(Server)
+    server.url     = opt.Url
+    server.headers = opt.Headers
+    server.body    = opt.Body
+    var client *http.Client
+    if opt.FollowRedirect {
+        client = &http.Client{
+            Transport: &http.Transport{},
+            CheckRedirect: func(req *http.Request, via []*http.Request) error {
+                return http.ErrUseLastResponse
+            },
+        }
+    } else {
+        client = &http.Client{
+            Transport: &http.Transport{},
+        }
+    }
+    server.client = client
+    return server
+}
+
+func (self *Server) setupHeaders() {
+    for k, v := range self.headers {
+        self.req.Header.Add(k, v)
+    }
+}
+
+func (self *Server) Get() (*Server) {
+    self.req, _ = http.NewRequest(GET, self.url, self.body)
+    self.setupHeaders()
+    return self
+}
+
+func (self *Server) Post() (*Server) {
+    self.req, _ = http.NewRequest(POST, self.url, self.body)
+    self.setupHeaders()
+    return self
+}
+
+func (self *Server) Exec() (r *ResponseReader, e error) {
+    o, e := self.client.Do(self.req)
+    r = &ResponseReader{o}
+    return
+}
+
+func (self *ResponseReader) Content() (body []byte, err error) {
+    defer self.Response.Body.Close()
+    body, err = ioutil.ReadAll(self.Response.Body)
+    return
+}
+
+func (self *ResponseReader) Text() (body string, err error) {
+    t, err := self.Content()
+    body = string(t)
+    return
+}
