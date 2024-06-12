e99446239934fa9bc048b933aabf02209135f124
Fadhil van Halen <rhyn.ml@gmailm.com>
2024-06-12 16:25:09 +0700

    ok,

--- api.go
+++ api.go
@@ -45,6 +45,13 @@ func (self *PsychopumpumApi) OpenAi(text string) map[string]interface{} {
     })
 }
 
+// Facebook Downloader
+func (self *PsychopumpumApi) FacebookDownload(url string) map[string]interface{} {
+    return self.Get("fb/dl/", map[string]string{
+        "url": url,
+    })
+}
+
 // Instagram
 func (self *PsychopumpumApi) InstagramProfile(username string) map[string]interface{} {
     return self.Get("instagram/post/", map[string]string{
@@ -60,6 +67,79 @@ func (self *PsychopumpumApi) InstagramPost(url string) map[string]interface{} {
 
 func (self *PsychopumpumApi) InstagramStory(url string) map[string]interface{} {
     return self.Get("instagram/story/", map[string]string{
+        "url": url,
+    })
+}
+
+// TikTok
+func (self *PsychopumpumApi) TikTokProfile(username string) map[string]interface{} {
+    return self.Get("tiktok/profile/", map[string]string{
+        "username": username,
+    })
+}
+
+func (self *PsychopumpumApi) TikTokDownload(url string) map[string]interface{} {
+    return self.Get("tiktok/download/", map[string]string{
+        "url": url,
+    })
+}
+
+// Pinterest 
+func (self *PsychopumpumApi) PinterestDownload(url string) map[string]interface{} {
+    return self.Get("pinterest/download/", map[string]string{
+        "url": url,
+    })
+}
+
+func (self *PsychopumpumApi) PinterestSimilar(url string) map[string]interface{} {
+    return self.Get("pinterest/similar/", map[string]string{
+        "url": url,
+    })
+}
+
+func (self *PsychopumpumApi) PinterestSearch(q string) map[string]interface{} {
+    return self.Get("pinterest/search/", map[string]string{
+        "q": q,
+    })
+}
+
+// YouTube
+func (self *PsychopumpumApi) YouTubeDownload(url string) map[string]interface{} {
+    return self.Get("youtube/download/", map[string]string{
+        "url": url,
+    })
+}
+
+func (self *PsychopumpumApi) YouTubeSearch(q, maxResult string) map[string]interface{} {
+    return self.Get("youtube/search/", map[string]string{
+        "q": q,
+        "maxResult": maxResult, // default 5
+    })
+}
+
+// RedTube
+func (self *PsychopumpumApi) RedTubeSearch(q string) map[string]interface{} {
+    return self.Get("redtube/search/", map[string]string{
+        "q": q,
+    })
+}
+
+func (self *PsychopumpumApi) RedTubeDownload(url string) map[string]interface{} {
+    return self.Get("redtube/download/", map[string]string{
+        "url": url,
+    })
+}
+
+// PornHub
+func (self *PsychopumpumApi) PornHubSearch(q, page string) map[string]interface{} {
+    return self.Get("pornhub/search/", map[string]string{
+        "q": q,
+        "page": page, // default 1
+    })
+}
+
+func (self *PsychopumpumApi) PornHubSearch(url string) map[string]interface{} {
+    return self.Get("pornhub/download/", map[string]string{
         "url": url,
     })
 }
